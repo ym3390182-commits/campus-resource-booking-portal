74ac2c93b5f20834b1623e516cd8abc80088e52d
Yash Mittal <ym3390182@gmail.com>
2026-08-01 11:58:33 +0530

    Add TiDB SSL configuration

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const app = require('./src/app');
 const { testDbConnection } = require('./src/config/database');
 const seedDefaultUsers = require('./src/database/seedAdmin');
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
 
 dotenv.config();
 
@@ -10,6 +12,16 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'campus_booking_db',
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+ // port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+
+ssl: {
+    ca: fs.readFileSync(
+        path.join(__dirname, '../../certs/isrgrootx1.pem')
+    ),
+    rejectUnauthorized: true
+},
+
+//waitForConnections: true,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
